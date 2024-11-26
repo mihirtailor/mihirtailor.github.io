6263a63c9fb5f48cdc440313f0942e79af01007d
Mihir Tailor <mihirtailor1994@gmail.com>
2024-11-25 19:51:24 -0500

    lifestyle hooks

--- parent_child_interaction/src/app/components/undergrads/undergrads.component.ts
+++ parent_child_interaction/src/app/components/undergrads/undergrads.component.ts
@@ -1,13 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChange,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'undergrads',
   templateUrl: './undergrads.component.html',
   styleUrl: './undergrads.component.css',
 })
-export class UndergradsComponent {
+export class UndergradsComponent implements OnChanges, OnInit, DoCheck {
   // accepts an arry of students using @Input() decorator
   @Input() undergradsList: any;
+  @Input() public textValue: any; // value of text input
 
   @Output() public childEvent = new EventEmitter();
   @Output() public deleteEvent = new EventEmitter();
@@ -15,6 +27,34 @@ export class UndergradsComponent {
   // method to emit event to parent component
   sendInfoToParent() {
     this.childEvent.emit('This is a message from child component');
+  }
+
+  // Lifestcycle hooks
+  // 1. constructor function runs first when component is created
+
+  constructor() {
+    console.log('undergrad constructor function');
+  }
+
+  // 2. ngOnChanges runs when there is a change in the input property
+  ngOnChanges(change: SimpleChanges): void {
+    console.log('undergrads ngOnChanges', change);
+  }
+
+  // 3. ngOnInit runs after only once after ngOnChanges
+  ngOnInit(): void {
+    console.log('undergrads ngOnInit');
+  }
+
+  // 4. ngDoCheck runs after ngOnInit: used to detect and act upon changes that
+  // Angular can't or won't detect on its own
+  ngDoCheck(): void {
+    console.log('undergrads ngDoCheck');
+  }
+
+  // 5. ngOnDestroy runs before the component is destroyed
+  ngOnDestroy(): void {
+    console.log('undergrads ngOnDestroy');
   }
 
   deleteStudent(id: number) {

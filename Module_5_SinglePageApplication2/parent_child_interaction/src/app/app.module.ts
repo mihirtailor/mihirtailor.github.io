230bf373399d3c7959ac282c1f3d9930bb3f2ec4
mihirtailor <mihirtailor1994@gmail.com>
2024-11-25 20:01:27 -0500

    lifestyle hooks

--- Module_5_SinglePageApplication2/parent_child_interaction/src/app/app.module.ts
+++ Module_5_SinglePageApplication2/parent_child_interaction/src/app/app.module.ts
@@ -7,10 +7,17 @@ import { StudentsComponent } from './components/students/students.component';
 import { UndergradsComponent } from './components/undergrads/undergrads.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, StudentsComponent, UndergradsComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    StudentsComponent,
+    UndergradsComponent,
+    HomeComponent,
+    NotFoundComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })

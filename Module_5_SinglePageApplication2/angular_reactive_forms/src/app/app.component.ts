68ce95ccb5a3f2119086568f3db3bdd56aeef412
Mihir Tailor <mihirtailor1994@gmail.com>
2024-11-26 21:04:28 -0500

    remove unnecessary import

--- Module_5_SinglePageApplication2/angular_reactive_forms/src/app/app.component.ts
+++ Module_5_SinglePageApplication2/angular_reactive_forms/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ReactiveformComponent } from './reactiveform/reactiveform.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ReactiveformComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

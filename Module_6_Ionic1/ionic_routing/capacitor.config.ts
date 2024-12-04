84a9d3b94b3f9a28ffb2b85d1f2ef198352792db
Mihir Tailor <mihirtailor1994@gmail.com>
2024-12-03 19:32:47 -0500

    ionic routing project

---
+++ Module_6_Ionic1/ionic_routing/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_routing',
+  webDir: 'www'
+};
+
+export default config;

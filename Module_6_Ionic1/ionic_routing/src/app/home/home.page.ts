7e16456614edd9a356a27830dffcda5b6704bf89
Mihir Tailor <mihirtailor1994@gmail.com>
2024-12-03 19:54:47 -0500

    prod live server

--- Module_6_Ionic1/ionic_routing/src/app/home/home.page.ts
+++ Module_6_Ionic1/ionic_routing/src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  appName = environment.appName;
+  apiEndpoint = environment.apiEndpoint;
   constructor() {}
-
 }
--- Module_6_Ionic1/ionic_routing/src/environments/environment.prod.ts
+++ Module_6_Ionic1/ionic_routing/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndpoint: 'https://prod-api.example.com',
+  appName: 'Ionic Prod App',
 };

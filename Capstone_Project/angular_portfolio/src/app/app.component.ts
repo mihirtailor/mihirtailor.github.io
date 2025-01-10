5464217e2b1a40680ecf308a31852a09d8814829
Mihir Tailor <mihirtailor1994@gmail.com>
2025-01-10 09:32:06 -0500

    add personal portfolio

---
+++ Capstone_Project/angular_portfolio/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { EducationComponent } from './components/education/education.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    EducationComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'angular_portfolio';
+}

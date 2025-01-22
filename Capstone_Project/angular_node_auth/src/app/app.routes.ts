40942d0d787a1bf39393de157ad6dd55fb62c8b4
Mihir Tailor <mihirtailor1994@gmail.com>
2025-01-22 13:46:52 -0500

    details component added

--- Capstone_Project/angular_node_auth/src/app/app.routes.ts
+++ Capstone_Project/angular_node_auth/src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 import { authGuard } from './auth.guard';
+import { DetailsComponent } from './components/details/details.component';
 
 export const routes: Routes = [
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    canActivate: [authGuard],
+  },
   { path: '*', redirectTo: '/signin', pathMatch: 'full' },
 ];
